efd0d30ea054ab45e377ad7d392c7ee63e828ee7
unknown <rrrr0ffff@gmail.com>
2022-11-30 00:41:24 -0300

    cors and helmet

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const helmet = require('helmet');
+const cors = require('cors');
 const express = require('express');
 const connect = require('./config/database.js');
 const PORT = process.env.PORT || 3334;
@@ -8,6 +10,8 @@ const todoRoute = require('./routes/todo.route.js');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors());
 //app.use('/api/user', userRoute);
 app.use('/api/todo', todoRoute);
 
