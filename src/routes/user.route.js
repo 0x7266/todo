768bc8f8acf551cf8305a7a3c84564e1a6fb58da
unknown <rrrr0ffff@gmail.com>
2022-12-01 15:15:47 -0300

    -

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,10 +2,11 @@ const { Router } = require('express');
 
 const router = Router();
 
-router.get('/', getUsers);
-router.get('/:id', getUser);
-router.post('/', createUser);
-router.patch('/:id', editUser);
-router.delete('/:id', deleteUser);
+// router.get('/', getUsers);
+// router.get('/:id', getUser);
+router.post('/login', loginUser);
+router.post('/signup', signupUser);
+// router.patch('/:id', editUser);
+// router.delete('/:id', deleteUser);
 
 module.exports = router;
